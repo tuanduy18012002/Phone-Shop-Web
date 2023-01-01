73027e1ed06d8100f93c358b6c368517f7b76617
Vũ Huy Hoàng <vhhoang20@clc.fitus.edu.vn>
2023-01-01 16:03:09 +0700

    guest permission

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -1,12 +1,17 @@
 const { request } = require('express')
 const product = require('../../models/product')
 const user = require('../../models/user')
+const passport = require('../../util/passport')
 const {multipleMongooseToObject} =  require('../../util/mongoose')
 
 class adminController
 {
     show(req, res, next)
     {
+        if (req.user == null)
+        {
+            res.redirect('/')
+        }
         const temp = req.user[0]
         const ad = JSON.parse(JSON.stringify(temp))
         if (ad.admin)
