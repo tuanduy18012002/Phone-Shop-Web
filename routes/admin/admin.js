84d3164d9b3ebb4b83373ea531b6830b9c86e386
Huynh Tuan Duy <74585439+tuanduy18012002@users.noreply.github.com>
2023-01-02 16:27:14 +0700

    dev bill-list, bill-detail and home page in admin

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var product_router = require('./product')
 var user_router = require('./user')
+var bill_router = require('./bill')
 var adminController = require('../../controllers/admin/adminController');
 
 router.use('/', function (req, res, next) {
@@ -10,6 +11,7 @@ router.use('/', function (req, res, next) {
   });
 router.use('/product', product_router)
 router.use('/user', user_router)
+router.use('/bill', bill_router)
 router.use('/', adminController.show)
 
 module.exports = router;
